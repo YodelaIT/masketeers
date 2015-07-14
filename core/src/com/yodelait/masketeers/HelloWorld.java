6049c01f7ee38e1d9284d577392c3c25e8f5de61
Thomas Dhondt <thomas.dhondt.u2588@student.hogent.be>
2015-07-14 23:36:06 +0200

    Some basic tutorials

--- core/src/com/yodelait/masketeers/HelloWorld.java
+++ core/src/com/yodelait/masketeers/HelloWorld.java
@@ -2,18 +2,23 @@ package com.yodelait.masketeers;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class HelloWorld extends ApplicationAdapter {
+	private SpriteBatch batch;
+	private Texture img;
+        private BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+                font = new BitmapFont();
+                font.setColor(Color.BLACK);
 	}
 
 	@Override
@@ -21,7 +26,8 @@ public class Main extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+                font.draw(batch, "Hello world!", 400,100);
+		batch.draw(img, 50, 0);
 		batch.end();
 	}
 }
--- desktop/src/com/yodelait/masketeers/desktop/DesktopLauncher.java
+++ desktop/src/com/yodelait/masketeers/desktop/DesktopLauncher.java
@@ -2,11 +2,13 @@ package com.yodelait.masketeers.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.yodelait.masketeers.Main;
+import com.yodelait.masketeers.GraphicsDemo;
+import com.yodelait.masketeers.HelloWorld;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Main(), config);
+		//new LwjglApplication(new HelloWorld(), config);
+                new LwjglApplication(new GraphicsDemo(), config);
 	}
 }
